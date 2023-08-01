346f69fccd2d4a04084cce7596ba35f24e7716c3
BlueSgler <3032964348@qq.com>
2023-08-01 23:47:47 +0800

    add login

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASEURL),
   routes: [
     {
       path: "/",
@@ -14,12 +14,12 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/login",
+      name: "login",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("../views/LoginView.vue"),
     },
   ],
 });
